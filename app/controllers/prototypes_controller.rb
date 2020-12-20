9b6b04dec70e3eddf388c3f68ef96e2e27cded09
takayamaryoutarou <on89takayama1126@gmail.com>
2020-12-20 14:55:17 +0900

    initial commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -13,6 +13,7 @@ class PrototypesController < ApplicationController
   
   def create
     @prototype = Prototype.new(prototype_params)
+    
     if @prototype.save
       redirect_to root_path
     else
