ee3dfac5f96cc9c23fd9639c86ca587d100f647b
takayamaryoutarou <on89takayama1126@gmail.com>
2020-12-21 17:01:03 +0900

    修正
    投稿者以外が投稿したURL入力でのサイト移動はリダイレクトされるように修正。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
